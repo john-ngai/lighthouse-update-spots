c93fb8508b1913edb744ffd2db621428d84eac02
mrjohnming <john.ngai@hotmail.com>
2022-03-01 20:16:19 -0500

    My solution

--- updateSpots.js
+++ updateSpots.js
@@ -8,11 +8,30 @@
  */
 
 const updateSpots = function(state, appointments, id) {
+  let index;
+  let appointmentIDs;
+  let spots = 0;
+
+  // Declare a copy (not reference) of the days array from states.
+  const days = [...state.days];
+  const selectedDay = state.day;
+
+  // Get the appointment IDs for the selected day.
+  days.forEach(day => {
+    if (day.name === selectedDay) {
+      index = day.id - 1;
+      appointmentIDs = day.appointments;
+    }
+  })
+
+  // Increment spots when the interview key-value for the selected appointment id is falsey (null).
+  appointmentIDs.forEach(id => !appointments[id].interview ? spots++ : null);
+
+  // Update the remaining spots for the selected day.
+  days[index] = {...days[index], spots};
 
   // return an updated days array 
-  return [];
+  return days;
 };
 
 module.exports = updateSpots;
-
-
